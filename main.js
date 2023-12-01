fddbf8fc50513af2163860750c89ecc539858d4a
kalisonk <106752910+kalisonk@users.noreply.github.com>
2023-12-01 10:16:55 -0300

    afadifg

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-for (let contador = 0; contador < listaDeTeclas.length; contador++){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
-    const idAudio = `#som_&{efeito}`;
+    const idAudio = `#som_${efeito}`;
     tecla.onliclick = function() {
         tocaSom(idAudio);
     };
